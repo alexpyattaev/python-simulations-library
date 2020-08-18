65c03ab846affa0328073160ae0b11b4256ca719
Alex Pyattaev <alex.pyattaev@gmail.com>
2020-08-18 14:20:41 +0300

    Everything works

--- digital_filters.py
+++ digital_filters.py
@@ -233,3 +233,19 @@ class TestLinearFilters(unittest.TestCase):
         plt.figure()
         plt.plot(x, y, '*')
         plt.plot(x, (x - x.max() / 2) * a + c, '-', label='linear')
+
+
+def rle(inarray):
+    """ run length encoding. Partial credit to R rle function.
+        Multi datatype arrays catered for including non Numpy
+        returns: tuple (runlengths, startpositions, values) """
+    ia = np.asarray(inarray)  # force numpy
+    n = len(ia)
+    if n == 0:
+        return None, None, None
+    else:
+        y = np.array(ia[1:] != ia[:-1])  # pairwise unequal (string safe)
+        i = np.append(np.where(y), n - 1)  # must include last element posi
+        z = np.diff(np.append(-1, i))  # run lengths
+        p = np.cumsum(np.append(0, z))[:-1]  # positions
+        return z, p, ia[i]
