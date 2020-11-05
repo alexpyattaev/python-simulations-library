0b301813e3dda6034400f1fe040ab9a52a6519f5
Alex Pyattaev <alex.pyattaev@gmail.com>
2020-11-05 19:44:13 +0200

    Additional validation for distance metrics

--- ml_utils.py
+++ ml_utils.py
@@ -59,6 +59,92 @@ def load_tensorflow_model(coeffs: str, model_skel: Union[tf.keras.models.Model, 
     return model
 
 
+
+class EarlyStopping(Callback):
+  """Abort training when strange patterns in TP/TN balance is found"""
+
+  def __init__(self,
+               monitor=('TP', 'TN'),
+               min_delta=0,
+               patience=0,
+               verbose=0,
+               mode='auto',
+               baseline=None,
+               restore_best_weights=False):
+    super(EarlyStopping, self).__init__()
+
+    self.monitor = monitor
+    self.patience = patience
+    self.verbose = verbose
+    self.baseline = baseline
+    self.min_delta = abs(min_delta)
+    self.wait = 0
+    self.stopped_epoch = 0
+    self.restore_best_weights = restore_best_weights
+    self.best_weights = None
+
+    if mode not in ['auto', 'min', 'max']:
+      logging.warning('EarlyStopping mode %s is unknown, '
+                      'fallback to auto mode.', mode)
+      mode = 'auto'
+
+    if mode == 'min':
+      self.monitor_op = np.less
+    elif mode == 'max':
+      self.monitor_op = np.greater
+    else:
+      if 'acc' in self.monitor:
+        self.monitor_op = np.greater
+      else:
+        self.monitor_op = np.less
+
+    if self.monitor_op == np.greater:
+      self.min_delta *= 1
+    else:
+      self.min_delta *= -1
+
+  def on_train_begin(self, logs=None):
+    # Allow instances to be re-used
+    self.wait = 0
+    self.stopped_epoch = 0
+    if self.baseline is not None:
+      self.best = self.baseline
+    else:
+      self.best = np.Inf if self.monitor_op == np.less else -np.Inf
+    self.best_weights = None
+
+  def on_epoch_end(self, epoch, logs=None):
+    current = self.get_monitor_value(logs)
+    if current is None:
+      return
+    if self.monitor_op(current - self.min_delta, self.best):
+      self.best = current
+      self.wait = 0
+      if self.restore_best_weights:
+        self.best_weights = self.model.get_weights()
+    else:
+      self.wait += 1
+      if self.wait >= self.patience:
+        self.stopped_epoch = epoch
+        self.model.stop_training = True
+        if self.restore_best_weights:
+          if self.verbose > 0:
+            print('Restoring model weights from the end of the best epoch.')
+          self.model.set_weights(self.best_weights)
+
+  def on_train_end(self, logs=None):
+    if self.stopped_epoch > 0 and self.verbose > 0:
+      print('Epoch %05d: early stopping' % (self.stopped_epoch + 1))
+
+  def get_monitor_value(self, logs):
+    logs = logs or {}
+    monitor_value = logs.get(self.monitor)
+    if monitor_value is None:
+      logging.warning('Early stopping conditioned on metric `%s` '
+                      'which is not available. Available metrics are: %s',
+                      self.monitor, ','.join(list(logs.keys())))
+    return monitor_value
+
 class TF_Reset_States_Callback(Callback):
     def __init__(self, batches_in_msmt: Union[int, Iterable[int]]):
         Callback.__init__(self)
