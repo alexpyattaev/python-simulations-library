5e66bfd560a8c9bed4a6b7cd2b95945ba3d4a013
Alex Pyattaev <alex.pyattaev@gmail.com>
2020-11-18 19:19:56 +0200

    objwalk

---
+++ objwalk.py
@@ -0,0 +1,26 @@
+__author__ = 'Yaniv Aknin'
+
+
+from collections import Mapping, Set, Sequence
+
+string_types = (str, bytes)
+iteritems = lambda mapping: getattr(mapping, 'iteritems', mapping.items)()
+
+
+def objwalk(obj, path=(), memo=None):
+    if memo is None:
+        memo = set()
+    iterator = None
+    if isinstance(obj, Mapping):
+        iterator = iteritems
+    elif isinstance(obj, (Sequence, Set)) and not isinstance(obj, string_types):
+        iterator = enumerate
+    if iterator:
+        if id(obj) not in memo:
+            memo.add(id(obj))
+            for path_component, value in iterator(obj):
+                for result in objwalk(value, path + (path_component,), memo):
+                    yield result
+            memo.remove(id(obj))
+    else:
+        yield path, obj
