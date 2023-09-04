c65075ee824e1822831fa99b03342b94f9e6d24c
Alex Pyattaev <alex.pyattaev@gmail.com>
2023-09-04 14:52:53 +0300

    fix missing type sentinel

--- arg.py
+++ arg.py
@@ -272,7 +272,8 @@ def parse_to(container_class: Type[T], epilog: str = "", transform_names: Callab
                 raise TypeError(f"Values must be typed as subclasses of Arg or be one of {autocast_types}")
         else:
             value = value_or_class
-            if default is not None and default_factory is None:
+            print(value, default, default_factory)
+            if default is not None and default_factory == dataclasses.MISSING:
                 value.set_default(default)
         if verbose:
             print("add_argument", mangle_name(name, value.pos), value.kwargs)
