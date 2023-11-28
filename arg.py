6848c57906b21a0b1ed8ec354e65d8728efd2e97
Alex Pyattaev <alex.pyattaev@gmail.com>
2023-11-28 17:45:55 +0200

    Fix unit tests

--- arg.py
+++ arg.py
@@ -7,12 +7,23 @@ from typing import Callable, Union, Dict, TypeVar, Type, Iterable
 import inspect
 import pytest
 
-__all__ = ('Arg', 'Int', 'Float', 'Str', 'Choice', 'File', 'Bool', 'List', 'parse_to',
-           'Arg_Container', 'Force_Annotation')
+__all__ = (
+    "Arg",
+    "Int",
+    "Float",
+    "Str",
+    "Choice",
+    "File",
+    "Bool",
+    "List",
+    "parse_to",
+    "Arg_Container",
+    "Force_Annotation",
+)
 
 
 class repr_override:
-    """ Provides enum-specific repr override for nice looks"""
+    """Provides enum-specific repr override for nice looks"""
 
     def __init__(self, v):
         self.v = v
@@ -39,24 +50,28 @@ class Force_Annotation:
 
     def __post_init__(self):
         for vn, v in self.__class__.__dict__.items():
-            if vn.startswith('__'):
+            if vn.startswith("__"):
                 continue
             if inspect.isfunction(v) or inspect.isdatadescriptor(v):
                 continue
-            assert vn in self.__annotations__, f"All variables in {self.__class__} must be annotated, {vn} was not!"
+            assert (
+                vn in self.__annotations__
+            ), f"All variables in {self.__class__} must be annotated, {vn} was not!"
 
 
 class Arg:
-    """Basic argument, type is not inferred, kwargs are passed to argparse """
+    """Basic argument, type is not inferred, kwargs are passed to argparse"""
 
-    def __init__(self, typ: Union[type, Callable[[str], object]], pos: bool = False, **kwargs):
+    def __init__(
+        self, typ: Union[type, Callable[[str], object]], pos: bool = False, **kwargs
+    ):
         """
         :param typ: type of data / function to convert from str to object
         :param pos: flag to indicate if positional
         :param kwargs: passed to argparse
         """
         kwargs = kwargs.copy()
-        kwargs['type'] = typ
+        kwargs["type"] = typ
         self.kwargs = kwargs
         self.pos = pos
 
@@ -66,15 +81,16 @@ class Arg:
         :param default:
         """
         if isinstance(default, Enum):
-            self.kwargs['default'] = repr_override(default)
+            self.kwargs["default"] = repr_override(default)
         else:
-            self.kwargs['default'] = default
+            self.kwargs["default"] = default
 
 
 class Int(Arg):
     """Int argument"""
 
-    def __init__(self, **kwargs):
+    def __init__(self, bounds=(None, None), **kwargs):
+        self.bounds = bounds
         Arg.__init__(self, typ=int, **kwargs)
 
 
@@ -88,8 +104,8 @@ class Bool(Arg):
         """
         self.flag = flag
         Arg.__init__(self, typ=bool, **kwargs)
-        if 'default' in kwargs:
-            self.set_default(kwargs['default'])
+        if "default" in kwargs:
+            self.set_default(kwargs["default"])
 
     def set_default(self, default):
         def strtobool(val):
@@ -100,9 +116,9 @@ class Bool(Arg):
             'val' is anything else.
             """
             val = val.lower()
-            if val in ('y', 'yes', 't', 'true', 'on', '1'):
+            if val in ("y", "yes", "t", "true", "on", "1"):
                 return True
-            elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+            elif val in ("n", "no", "f", "false", "off", "0"):
                 return False
             else:
                 raise ValueError("invalid truth value %r" % (val,))
@@ -111,24 +127,25 @@ class Bool(Arg):
             try:
                 # This is ok to ignore since same instance may be used to parse many times,
                 # in which case this is already done
-                self.kwargs.pop('type')
+                self.kwargs.pop("type")
             except KeyError:
                 pass
             if default is True:
-                self.kwargs['action'] = 'store_false'
+                self.kwargs["action"] = "store_false"
             elif default is False:
-                self.kwargs['action'] = 'store_true'
+                self.kwargs["action"] = "store_true"
             else:
-                raise ValueError('Bool flags must have default set!')
+                raise ValueError("Bool flags must have default set!")
         else:
-            self.kwargs['type'] = strtobool
+            self.kwargs["type"] = strtobool
             Arg.set_default(self, default)
 
 
 class Float(Arg):
     """Float argument"""
 
-    def __init__(self, **kwargs):
+    def __init__(self, bounds=(None, None), **kwargs):
+        self.bounds = bounds
         Arg.__init__(self, typ=float, **kwargs)
 
 
@@ -155,7 +172,10 @@ class List(Arg, metaclass=_MetaList):
      " --arg 1 --arg 2 --arg 3 --arg 4 "
     """
 
-    def __init__(self, **kwargs, ):
+    def __init__(
+        self,
+        **kwargs,
+    ):
         Arg.__init__(self, **kwargs)
 
     def __call__(self, **kwargs):
@@ -174,6 +194,7 @@ class _MetaChoice(type):
             choices = [repr_override(f) for f in item]
             extra_help = "; ".join(f"{f.name}: {f.value}" for f in item)
             if issubclass(item, IntEnum):
+
                 def typ(x):
                     try:
                         return getattr(item, x)
@@ -182,13 +203,20 @@ class _MetaChoice(type):
                             return item(int(x))
                         except ValueError:
                             pass
-                    raise argparse.ArgumentTypeError(f"invalid {item.__name__} value: {x}")
+                    raise argparse.ArgumentTypeError(
+                        f"invalid {item.__name__} value: {x}"
+                    )
+
             else:
+
                 def typ(x):
                     try:
                         return getattr(item, x)
                     except AttributeError:
-                        raise argparse.ArgumentTypeError(f"invalid {item.__name__} value: {x}")
+                        raise argparse.ArgumentTypeError(
+                            f"invalid {item.__name__} value: {x}"
+                        )
+
         else:
             choices = list(item)
             typ = type(item[0])
@@ -199,20 +227,22 @@ class _MetaChoice(type):
 class Choice(Arg, metaclass=_MetaChoice):
     """Choice out of iterable or Enum subclass.
 
-    If enum is given as argument, the names of the fields will be used, and values will be returned.
+    If enum is given as argument, the names of the fields will be used, 
+    and values will be returned.
     """
 
     def __init__(self, choices, extra_help=None, **kwargs):
         self.extra_help = extra_help
+        self.choices = choices
         Arg.__init__(self, choices=choices, **kwargs)
 
     def __call__(self, **kwargs):
-        assert 'choices' not in kwargs
+        assert "choices" not in kwargs
         if self.extra_help is not None:
-            if 'help' in kwargs:
-                kwargs['help'] += ' [' + self.extra_help + ']'
+            if "help" in kwargs:
+                kwargs["help"] += " [" + self.extra_help + "]"
             else:
-                kwargs['help'] = '[' + self.extra_help + ']'
+                kwargs["help"] = "[" + self.extra_help + "]"
 
         self.kwargs.update(kwargs)
 
@@ -222,20 +252,53 @@ class Choice(Arg, metaclass=_MetaChoice):
 class File(Arg):
     """File argument"""
 
-    def __init__(self, mode='r', bufsize=-1, encoding=None, errors=None, **kwargs):
-        Arg.__init__(self, typ=argparse.FileType(mode, bufsize, encoding, errors), **kwargs)
+    def __init__(self, mode="r", bufsize=-1, encoding=None, errors=None, **kwargs):
+        Arg.__init__(
+            self, typ=argparse.FileType(mode, bufsize, encoding, errors), **kwargs
+        )
 
 
-autocast_types = {int: Int,
-                  float: Float,
-                  str: Str,
-                  bool: Bool}
-
-T = TypeVar('T')
+autocast_types = {int: Int, float: Float, str: Str, bool: Bool}
 
 
-def parse_to(container_class: Type[T], epilog: str = "", transform_names: Callable[[str], str] = None,
-             verbose: bool = False, args=None) -> T:
+@dataclasses.dataclass
+class Arg_Container(Force_Annotation):
+    """Argument Container class"""
+
+    def asdict(self) -> Dict[str, object]:
+        result = {}
+        for f in dataclasses.fields(self):
+            value = getattr(self, f.name)
+            if isinstance(value, IOBase) and hasattr(value, "name"):
+                value = value.name
+            result[f.name] = value
+        return result
+
+    def to_json(self):
+        result = {}
+        for f in dataclasses.fields(self):
+            value = getattr(self, f.name)
+            if isinstance(value, IOBase):
+                if hasattr(value, "name"):
+                    value = value.name
+                else:
+                    raise TypeError(f"Could not convert filed {f.name}={value}")
+            if isinstance(value, Enum):
+                value = value.name
+            result[f.name] = value
+        return result
+
+
+A = TypeVar("A", Arg_Container, Arg_Container)
+
+
+def parse_to(
+    container_class: Type[A],
+    epilog: str = "",
+    transform_names: Callable[[str], str] = None,
+    verbose: bool = False,
+    args=None,
+) -> A:
     """
     Parse command line using argparse into the provided container class.
 
@@ -246,6 +309,10 @@ def parse_to(container_class: Type[T], epilog: str = "", transform_names: Callab
     :param args: passed verbatim to ArgumentParser.parse_args
     :return: container_class filled in with parsed args.
     """
+    assert issubclass(
+        container_class,
+        Arg_Container,
+    ), "container_class should be a subclass of Arg_Container"
 
     def mangle_name(n: str, positional: bool):
         s = "" if positional else "--"
@@ -253,7 +320,6 @@ def parse_to(container_class: Type[T], epilog: str = "", transform_names: Callab
             n = transform_names(n)
         return s + n
 
-    assert dataclasses.is_dataclass(container_class), "container_class should be a dataclass"
     parser = argparse.ArgumentParser(description=container_class.__doc__, epilog=epilog)
 
     for field in dataclasses.fields(container_class):
@@ -261,7 +327,9 @@ def parse_to(container_class: Type[T], epilog: str = "", transform_names: Callab
         default = field.default
         default_factory = field.default_factory
         value_or_class = field.type
-        if isinstance(value_or_class, type):  # Type is not an instance (e.g. int or float)
+        if isinstance(
+            value_or_class, type
+        ):  # Type is not an instance (e.g. int or float)
             if issubclass(value_or_class, Arg):
                 # noinspection PyArgumentList
                 value = value_or_class(default=default)
@@ -269,7 +337,9 @@ def parse_to(container_class: Type[T], epilog: str = "", transform_names: Callab
                 # noinspection PyTypeChecker
                 value = autocast_types[value_or_class](default=default)  # type: ignore
             else:
-                raise TypeError(f"Values must be typed as subclasses of Arg or be one of {autocast_types}")
+                raise TypeError(
+                    f"Values must be typed as subclasses of Arg or be one of {autocast_types}"
+                )
         else:
             value = value_or_class
             if default is not None and default_factory == dataclasses.MISSING:
@@ -287,38 +357,11 @@ def parse_to(container_class: Type[T], epilog: str = "", transform_names: Callab
     return container_class(**corrected_dict)
 
 
-@dataclasses.dataclass
-class Arg_Container(Force_Annotation):
-    """Argument Container class"""
-
-    def asdict(self) -> Dict[str, object]:
-        result = {}
-        for f in dataclasses.fields(self):
-            value = getattr(self, f.name)
-            if isinstance(value, IOBase) and hasattr(value, 'name'):
-                value = value.name
-            result[f.name] = value
-        return result
-
-    def to_json(self):
-        result = {}
-        for f in dataclasses.fields(self):
-            value = getattr(self, f.name)
-            if isinstance(value, IOBase):
-                if hasattr(value, 'name'):
-                    value = value.name
-                else:
-                    raise TypeError(f"Could not convert filed {f.name}={value}")
-            if isinstance(value, Enum):
-                value = value.name
-            result[f.name] = value
-        return result
-
-
 @pytest.fixture
 def arg_definitions():
     class str_enum(Enum):
         """Enum of strings of things"""
+
         A = "All the things"
         B = "Best of things"
 
@@ -329,29 +372,30 @@ def arg_definitions():
     @dataclasses.dataclass
     class Args(Arg_Container):
         """Example of description for your application"""
+
         req_str: Str(help="required str field") = "boo"
 
         opt_str: Str(help="str field") = "bla"
         bare_str: str = "aaa"
 
-        int_field: Int(help='Int field') = 120
+        int_field: Int(help="Int field") = 120
         bare_int: int = 150
 
-        float_field: Float(help='Float field') = 10.0
+        float_field: Float(help="Float field") = 10.0
         bare_float: float = 15.0
 
         str_enum_field: Choice[str_enum](help="choice from string enum") = str_enum.A
         int_enum_field: Choice[int_enum](help="choice from int enum") = int_enum.TWO
 
         list_choice: Choice[[1, 4, 7]](help="choice from iterable") = 0
-        list_of_int: List[int](help="List of integers") = dataclasses.field(default_factory=list)
+        list_of_int: List[int](help="List of integers") = dataclasses.field(
+            default_factory=list
+        )
         bool_field: bool = False
         bool_flag: Bool(flag=True) = False
         bool_switch: Bool(flag=False) = False
 
     return Args
-
-
 
 
 def test_parse(arg_definitions):
